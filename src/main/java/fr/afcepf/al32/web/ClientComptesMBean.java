81199250500b231bc9cb708a7cfd61c8e2fe94fa
didier-defrance <didier@d-defrance.fr>
2018-10-02 16:33:40 +0200

    debut spring-jsf

--- src/main/java/fr/afcepf/al32/web/ClientComptesMBean.java
+++ src/main/java/fr/afcepf/al32/web/ClientComptesMBean.java
@@ -1,7 +1,13 @@
 package fr.afcepf.al32.web;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+
+import fr.afcepf.al32.entity.Compte;
+import fr.afcepf.al32.service.IServiceCompte;
 
 @ManagedBean
 @SessionScoped //ou @RequestScope
@@ -9,11 +15,14 @@ public class ClientComptesMBean {
 	
 	private Long numClient; //à saisir
 	
-	//private List<Compte> listeComptes;
+	@ManagedProperty("#{serviceCompteImpl}") //#{nomComposantJsfOuSpring} //nomClasseJava avec minuscule au debut
+	private IServiceCompte serviceCompte;//avec get?/set
+	
+	private List<Compte> listeComptes; //à afficher
 	
 	public String login() {
 		String suite=null;
-		
+		listeComptes = serviceCompte.rechercherComptesDuClient(numClient);
 	    suite="comptes"; // pour naviguer vers comptes.jsf (.xhtml ou .jsp)
 		return suite; 
 	}
@@ -24,6 +33,22 @@ public class ClientComptesMBean {
 
 	public void setNumClient(Long numClient) {
 		this.numClient = numClient;
+	}
+
+	public IServiceCompte getServiceCompte() {
+		return serviceCompte;
+	}
+
+	public void setServiceCompte(IServiceCompte serviceCompte) {
+		this.serviceCompte = serviceCompte;
+	}
+
+	public List<Compte> getListeComptes() {
+		return listeComptes;
+	}
+
+	public void setListeComptes(List<Compte> listeComptes) {
+		this.listeComptes = listeComptes;
 	}
 	
 	
